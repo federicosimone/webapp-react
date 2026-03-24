0a5cf41833291ff12bd447ea3e850bbb1616ec04
federicosimone <federico.simone15@gmail.com>
2026-03-24 10:27:27 +0100

    sistemazione layout

--- pages/MoviesList.jsx
+++ pages/MoviesList.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom"
 import FilmCard from "../components/FilmCard"
 import Movies from "../data/exampleList"
+import style from "./MoviesList.module.css"
 
 
 function MoviesList() {
@@ -8,14 +10,19 @@ function MoviesList() {
         <>
 
             <div className="container pt-5 pb-5">
-                <h1>Seleziona il film di cui vuoi leggere le informazioni</h1>
-                <div className="d-flex flex-wrap gap-4">
+                <div className="d-flex justify-content-between">
+                    <h1 className="d-inline">Seleziona il film di cui vuoi leggere le informazioni</h1>
+                    <Link className="btn btn-danger mb-3" to={`/`}>Torna alla Home</Link>
+                </div>
+                <div className="d-flex flex-wrap gap-3">
 
                     {Movies.map((movie) => (
-                        <FilmCard
-                            id={movie.id}
-                            title={movie.title}
-                            abstract={movie.abstract} />
+                        <div key={movie.id}>
+                            <FilmCard
+                                id={movie.id}
+                                title={movie.title}
+                                abstract={movie.abstract} />
+                        </div>
                     ))
                     }
                 </div>
